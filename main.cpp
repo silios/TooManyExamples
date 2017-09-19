6a6dadea128e0e92a8987bc665a45c5c72b96489
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-09-19 17:49:20 +0300

    nested for loop

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@
 using namespace std;
 
 void example1() {
-
   cout << "Example1() was called" << "\n\n";
+  for(int i = 0; i < 20; i++) {
+    for(int j = 0; j < i; j++) {
+      cout << j;
+    }
+    cout << endl;
+
+  }
 }
 
 
@@ -21,24 +27,24 @@ void example2() {
 int main() {
   int selection;
   cout << "\tWelcome to Too Many Examples :D\n\n";
-  cout << "Choose an example \n";
+  cout << "Choose an example 1,2,3 etc..\n";
   cin >> selection;
 
- // while (selection != 0) {
+  // while (selection != 0) {
 
 
-    switch (selection) {
-      case 1:
-        example1();
-        break;
-      case 2:
-        example2();
+  switch (selection) {
+    case 1:
+      example1();
+      break;
+    case 2:
+      example2();
 
 
-        break;
-      default:
-        break;
-    }
+      break;
+    default:
+      break;
+  }
   //}
   cout << "\t\t!!GAME OVER!!\n\n";
   return 0;
