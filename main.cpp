5a9f77e2e79ef1e1a6d350ea7a77ca8845634a0b
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-09-19 16:55:35 +0300

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
+//GuessMyNumber - The classic number guessing game
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+void example1() {
+
+  cout << "Example1() was called" << "\n\n";
 }
+
+
+void example2() {
+  cout << "Example2() was called" << "\n\n";
+}
+
+
+int main() {
+  int selection;
+  cout << "\tWelcome to Too Many Examples :D\n\n";
+  cout << "Choose an example \n";
+  cin >> selection;
+
+ // while (selection != 0) {
+
+
+    switch (selection) {
+      case 1:
+        example1();
+        break;
+      case 2:
+        example2();
+
+
+        break;
+      default:
+        break;
+    }
+  //}
+  cout << "\t\t!!GAME OVER!!\n\n";
+  return 0;
+}
+
