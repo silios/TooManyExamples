81839dada41a197720b2d12307f347a97bf4e23f
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-09-20 11:42:43 +0300

    Added printing triangle,inverted triangle and pyramid

--- main.cpp
+++ main.cpp
@@ -1,33 +1,84 @@
 //GuessMyNumber - The classic number guessing game
 
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <windows.h>
+
 
 using namespace std;
 
-void example1() {
-  cout << "Example1() was called" << "\n\n";
-  for(int i = 0; i < 20; i++) {
-    for(int j = 0; j < i; j++) {
-      cout << j;
-    }
-    cout << endl;
+void HalfPyramid() {
+  int rows;
+  int printType;
 
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  cout << "Print with numbers(1) or *(2): ";
+  cin >> printType;
+  cout << "\n";
+
+  for(int i = 1; i <= rows; ++i) {
+    for(int j = 1; j <= i; ++j) {
+      if(printType == 1) {
+        cout << j;
+      } else {
+        cout << "*";
+      }
+    }
+    cout << "\n";
   }
 }
 
 
-void example2() {
-  cout << "Example2() was called" << "\n\n";
+void InvertedHalfPyramid() {
+  int rows;
+  int printType;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  cout << "Print with numbers(1) or *(2): ";
+  cin >> printType;
+  cout << "\n";
+
+  for(int i = rows; i > 1; i--) {
+    for(int j = 1; j <= i; ++j) {
+      if(printType == 1) {
+        cout << j;
+      } else {
+        cout << "*";
+      }
+    }
+    cout << "\n";
+  }
 }
 
+void Pyramid() {
+  int space, rows;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+  cout << "\n";
+
+  for(int i = 1, k = 0; i <= rows; ++i, k = 0) {
+    for(space = 1; space <= rows - i; ++space) {
+      cout << "  ";
+    }
+
+    while(k != 2 * i - 1) {
+      cout << "* ";
+      ++k;
+    }
+    cout << endl;
+  }
+}
 
 int main() {
   int selection;
-  cout << "\tWelcome to Too Many Examples :D\n\n";
-  cout << "Choose an example 1,2,3 etc..\n";
+  cout << "Welcome to Too Many Examples :D\n\n";
+  cout << "Choose an example : \n";
+  cout << "1 - Print half pyramid with numbers or *\n";
+  cout << "2 - Print inverted half pyramid with number or *\n";
+  cout << "3 - Print pyramid *\n";
   cin >> selection;
 
   // while (selection != 0) {
@@ -35,18 +86,19 @@ int main() {
 
   switch (selection) {
     case 1:
-      example1();
+      HalfPyramid();
       break;
     case 2:
-      example2();
-
+      InvertedHalfPyramid();
+    case 3:
+      Pyramid();
 
       break;
     default:
       break;
   }
   //}
-  cout << "\t\t!!GAME OVER!!\n\n";
+  cout << "\n\n\t!!DONE!!\n\n";
   return 0;
 }
 
